964c7b643bf293a88192df55f657896e5e5ac158
Mateusz Tadla <mtadla@sii.pl>
2021-11-20 12:32:13 +0100

    add first test

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,27 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void shouldPass() {
+        Assert.assertEquals(1, 1);
+    }
+
+    @Test
+    public void shouldFail() {
+        Assert.assertEquals(1, 2);
+    }
+
+    @BeforeMethod
+    public void setup() {
+        System.out.println("I will be executed before each test");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        System.out.println("I will be executed after each test");
+    }
+}
